720b8c22a1ba61642aa96ba1967937069bb36860
yeon <yeon.kim.0317@gmail.com>
2020-11-30 20:06:53 +0900

    add habit tracker

---
+++ src/app.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import './app.css';
+//import Habit from './components/habit';
+import Habits from './components/habits';
+
+function App() {
+  return <Habits />;
+}
+
+export default App;
+
