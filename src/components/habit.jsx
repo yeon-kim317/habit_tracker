f2d766c83c52c514c659bf1a662eec572aa69c9b
yeon <yeon.kim.0317@gmail.com>
2020-12-09 21:43:09 +0900

    modified

--- src/components/habit.jsx
+++ src/components/habit.jsx
@@ -1,42 +1,84 @@
-import React, { Component } from 'react';
+import React, { PureComponent } from 'react';
 
-class Habit extends Component {
-    state = {
-        count: 0,
-
-    }
+class Habit extends PureComponent {
+    inputRef = React.createRef();
 
     handleIncrement = () => {
-        //state 오브젝트 안에 있는 count를 증가 한 뒤 state를 업데이트 해야 함
-        this.setState({
-            count: this.state.count + 1
-        });
-    }
+        this.props.onIncrement(this.props.habit);
+    };
+
     handleDecrement = () => {
-        const count = this.state.count - 1;
-        this.setState({
-            count: count < 0 ? 0 : count
-        });
+        this.props.onDecrement(this.props.habit);
+    };
+
+    handleDelete = () => {
+        this.props.onDelete(this.props.habit);
+    };
+
+    handleEdit = () => {
+        this.props.onEdit(this.props.habit);
+    };
+
+    handleComplete = () => {
+        const name = this.inputRef.current.value;
+        this.props.onComplete(this.props.habit, name);
     }
 
     render() {
-        //const habitName = this.props.habit.name;
-        const {name, count} = this.props.habit;
-        return(
-            <li className="habit">
-                <span className="habit-name">{name}</span>
-                <span className="habit-count">{count}</span>
-                <button className="habit-button habit-increase" onClick={this.handleIncrement}>
-                    <i className="fas fa-plus-square"></i>
-                </button>
-                <button className="habit-button habit-decrease" onClick={this.handleDecrement}>
-                    <i className="fas fa-minus-square"></i>
-                </button>
-                <button className="habit-button habit-delete">
-                    <i className="fas fa-trash"></i>
-                </button>
+        const {name, count, editMode} = this.props.habit;
+        const input = '<input type="text" value="">';
+        return (
+            <li className={`habit${editMode === 'false' ? '' : ' editMode'}`}>
+                <span className="left">
+                    <span className="habit-count">{count}</span>
+                    {editMode === 'false'
+                        ?  (<>
+                                <span className="habit-name">{name}</span>
+                                <button
+                                    className="habit-button habit-increase" 
+                                    onClick={this.handleIncrement}
+                                    >
+                                    <i className="fas fa-plus-square"></i>
+                                </button>
+                                <button 
+                                    className="habit-button habit-decrease"
+                                    onClick={this.handleDecrement}
+                                    >
+                                    <i className="fas fa-minus-square"></i>
+                                </button>
+                            </>
+                        )
+                        :  (<input ref={this.inputRef} type="text" defaultValue={name} className="edit-input"></input>)
+                    }
+                </span>
+                <span className="right">
+                    {editMode === 'false'
+                        ?  (<>
+                                <button
+                                    className="habit-button habit-edit"
+                                    onClick={this.handleEdit}
+                                >
+                                    <i className="far fa-edit"></i>
+                                </button>
+                                <button
+                                    className="habit-button habit-delete"
+                                    onClick={this.handleDelete}
+                                >
+                                    <i className="far fa-trash-alt"></i>
+                                </button>
+                            </>
+                        )
+                        :  (<button 
+                                className="habit-button habit-complete"
+                                onClick={this.handleComplete}
+                            >
+                                    <i className="fas fa-check"></i>
+                            </button>
+                        )
+                    }
+                </span>
             </li>
-        )
+        );
     }
 }
 
--- src/components/habits.jsx
+++ src/components/habits.jsx
@@ -1,22 +1,33 @@
 import React, { Component } from 'react';
 import Habit from './habit';
+import HabitAddForm from './habitAddForm';
 
 class Habits extends Component {
-    state = {
-        habits: [
-            { id: 1, name: 'Reading', count: 0},
-            { id: 2, name: 'Running', count: 0},
-            { id: 3, name: 'Coding', count: 0}
-        ]
-    }
-    
+    handleAdd = name => {
+        this.props.onAdd(name);
+    };
+
     render() {
         return (
-            <ul>
-                {this.state.habits.map(habit => (
-                    <Habit key={habit.id} habit={habit}/>
-                ))}
-            </ul>
+            <div className="contents">
+                <HabitAddForm onAdd={this.handleAdd} />
+                <ul className="habits">
+                    {this.props.habits.map(habit => (
+                        <Habit 
+                            key={habit.id}
+                            habit={habit}
+                            onIncrement={this.props.onIncrement}
+                            onDecrement={this.props.onDecrement}
+                            onDelete={this.props.onDelete}
+                            onEdit={this.props.onEdit}
+                            onComplete={this.props.onComplete}
+                        />
+                    ))}
+                </ul>
+                <button className="habits-reset" onClick={this.props.onReset}>
+                    <i className="fas fa-redo-alt"></i>Reset All
+                </button>
+           </div>
         );
     }
 }
